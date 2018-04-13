6c30789a06dba9730db790407ca6a51c8acfdbee
Etienne Camenen <etienne.camenen@gmail.com>
2018-04-13 16:38:06 +0200

    Refactoring

--- fingerprint_clustering.R
+++ fingerprint_clustering.R
@@ -8,6 +8,7 @@ nb_metabolites=9
 max_cluster=6
 margin=par(mar=c(5, 4, 4, 2) + 1.1)
 interval=1
+typeClassif=2
 #max_cluster=nb_metabolites/1.5
 #choix du niveau de coupure
 def=par()
@@ -45,14 +46,22 @@ distance_matrix=as.dist(data)
 ################################
 #          Clustering
 ################################
-classif=hclust(distance_matrix,method="ward.D2")
-#classif=hclust(distance_matrix,method="complete")
-#classif=hclust(distance_matrix,method="median")
+if (typeClassif==1){
+  classif=pam(data,3,diss=F,stand=F)
+}else if (typeClassif==2){
+  classif=kmeans(data,centers=4,nstart=100)
+}else if (typeClassif==3){
+  classif=hclust(distance_matrix,method="ward.D2")
+}else if (typeClassif==4){
+  classif=hclust(distance_matrix,method="complete")
+}else if (typeClassif==5){
+  classif=hclust(distance_matrix,method="median")
+}
 
 #par(mfrow=c(2,2)); plot(dendro);plot(dendro2);plot(dendro3);plot(dendro4);par(mfrow=c(1,1))
 
 #automaticly ordering by clusters
-classif = reorder.hclust(classif, data)
+if(typeClassif>2) classif = reorder.hclust(classif, data)
 
 ################################
 #          Cophenetic
@@ -69,54 +78,8 @@ plotCohenetic=function(){
   abline(0,1,col="grey",lwd=font_size,lty=2)
 }
 
-plotCohenetic()
+if(typeClassif>2) plotCohenetic()
 
-################################
-#          Height difference
-################################
-
-#Show differences between nodes levels (distance between clusters)
-getHeightDifference=function(){
-  height_classif=as.matrix(classif$height)
-  height_diff=matrix(0, length(height_classif), 1)
-  for (i in 2:(length(height_classif))){
-    height_diff[i,]=height_classif[i,]-height_classif[i-1,]
-  }
-  rownames(height_diff)=c((length(height_classif)+1):2)
-  return(height_diff)
-}
-height_diff=getHeightDifference()
-#matrix_height=cbind(height_classif,height_diff)
-#colnames(matrix_height)=c("node height","difference")
-
-getRankedHeight=function(){
-  ranked_height_diff=data.frame(height_diff)
-  ranked_height_diff=ranked_height_diff[order(-ranked_height_diff), , drop = FALSE]
-  rownames(ranked_height_diff)
-  return(ranked_height_diff)
-}
-ranked_height_diff=getRankedHeight()
-
-optimal_nb_clusters=as.numeric(rownames(ranked_height_diff)[1])
-#optimal_nb_clusters=nrow(data)-(which.max(getHeightDifference()[-(nrow(data)-1)])-1)
-
-getClusters= function(nb_clusters) {
-  cutree(classif,nb_clusters)
-}
-
-clusters=getClusters(optimal_nb_clusters)
-#clusters<-as.factor(cutree(dendro3,nb_clusters))
-
-getClusterSizes=function(){
-  cluster_sizes=table(clusters)
-  cluster_sizes=data.frame(cluster_sizes)[,2]
-  names(cluster_sizes)=paste("G",seq(1:optimal_nb_clusters),sep="")
-  cluster_sizes=data.frame(cluster_sizes)
-  names(cluster_sizes)="Effectif"
-  return(cluster_sizes)
-}
-cluster_sizes=getClusterSizes()
-print(cluster_size)
 
 ################################
 #          Fusion levels
@@ -165,7 +128,7 @@ getGroupContent=function(){
 plot_fusion_levels = function() {
   par(margin);#x11()
   subset_height=classif$height[(nrow(data)-max_cluster):(nrow(data)-1)]
-  plot(classif$height, nrow(data):2, type="S",main="Distance before each fusion level",cex.main=2,cex.lab=1.5,lwd=font_size,xlim=c(min(subset_height),max(subset_height)),ylim=c(2,max_cluster),font.lab=2,ylab="Number of clusters", xlab="Node height", sub="(in red, difference in height with the previous fusion level)",col="grey", axes=F)
+  plot(classif$height, nrow(data):2, type="b",main="Distance between each clustering event (=fusion)",cex.main=2,cex.lab=1.5,lwd=font_size,xlim=c(min(subset_height),max(subset_height)),ylim=c(2,max_cluster),font.lab=2,ylab="Number of clusters", xlab="Node height", sub="(in red, difference in height with the previous fusion level)",col="grey", axes=F)
   axis(2, seq(2,max_cluster),lwd=2,font.axis=font_size)
   axis(1,seq(round(min(subset_height)),round(max(subset_height))),lwd=2,font.axis=font_size)
   result=matrix(0,nrow=(nrow(data)-1),ncol=2) #inialize an array for the result
@@ -177,89 +140,102 @@ plot_fusion_levels = function() {
 plot_fusion_levels()
 
 ################################
-#          Dendrogram
-################################
-
-plotDendrogram=function(nb_clusters){
-  par(margin);#x11()
-  plot(classif, ylim=c(0,max(classif$height)),xlim=c(0,nrow(data)),hang=-1, cex.main=2, cex.lab=1.5,lwd=font_size,xlab="Metabolites", sub="",ylab="Distance before each fusion",main="Dendrogram",font.lab=font_size,axes=F)
-  axis(2, seq(0,max(classif$height)),lwd=font_size,font.axis=font_size,cex.axis=0.8)
-  #abline(h=seq(0.0,max(classif$height),0.1), lty=3, col="grey")
-  #abline(h=c(classif$height), lty=3, col="grey")
-  #projection of the clusters
-  rect.hclust(classif, k=nb_clusters, border=rainbow(nb_clusters))
-}
-
-x11();plotDendrogram(as.numeric(optimal_nb_clusters))
-
-################################
-#          Silhouette
-################################
-
-plotAllSilhouette=function(max_cluster){
-  asw <- numeric(max_cluster-1)
-  for (k in 2:(max_cluster-1)) {
-    sil <- silhouette(cutree(classif, k=k), distance_matrix)
-    asw[k] <- summary(sil)$avg.width
-  }
-  
-  k.best <- which.max(asw)
-  # The plot is produced by function plot.silhouette {cluster}
-  plot(1:(max_cluster-1), asw, type="b", xlim=c(2,(max_cluster-1)),ylim=c(0,max(asw)+0.1),col="grey",main="Silhouette-optimal number of clusters, Ward",xlab="Number of groups", ylab="Average silhouette width", axes=F)
-  text(k.best,max(asw),paste("optimum",k.best,sep=":"),col="red", pos=2)
-  axis(1, seq(2,(max_cluster-1)),lwd=font_size,font.axis=font_size,cex.axis=0.8)
-  axis(2, seq(0.0,(max(asw)+0.1),0.1),lwd=font_size,font.axis=font_size,cex.axis=0.8)
-  points(k.best, max(asw), pch=21, col="red", cex=1)
-  cat("","Silhouette-optimal number of clusters k =", k.best, "\n","with an average silhouette width of", round(max(asw),4), "\n")
-  abline(v=k.best,lty=2,col="red")
-}
-
-plotSmallClusterSilhouette=function(max_cluster){
-  par(mfrow=c(2,2))
-  for (k in 3:max_cluster) {
-    sil <- silhouette(cutree(classif, k=k), distance_matrix)
-    silo <- sortSilhouette(sil)
-    rownames(silo) <- row.names(data)[attr(silo,"iOrd")]
-    plot(silo, main=paste("Silhouette plot for",k ,"groups"),cex.names=0.8, col=silo[,1]+1, nmax.lab=100)
-  }
-  par(mfrow=c(1,1))
-}
-
-plotAllSilhouette(max_cluster)
-plotSmallClusterSilhouette(max_cluster)
-
-################################
 #          Inertie inter-
 ################################
 
-getInertieInter=function(classif,k) {
-  sum_inertia <- 0;
-  element=length(classif$label)-1 ; imax <- k-1
+getCumulatedInertieInter = function(classif, nb_cluster) {
+  sum_inertia = 0
+  element = length(classif$label) - 1
+  imax = nb_cluster - 1
   for (i in 1:imax) {
-    sum_inertia=sum_inertia+classif$height[element]
-    element=element-1
+    sum_inertia = sum_inertia + classif$height[element]
+    element = element-1
   }
-  inertia=1000*sum_inertia/sum(classif$height) ; inertia=round(inertia)/10
+  inertia = 100 * sum_inertia / sum(classif$height)
+  inertia = round(inertia,2)
   return(inertia)
 }
 
-plotInertiaInter=function(max_cluster){
-  inertia=vector(mode="numeric",max_cluster-1)
-  for (k in 2:max_cluster){
-    inertia[k-1] = getInertieInter(classif,k)
+getCumulatedInertiaInterPerCluster=function(max_cluster){
+  inertia=vector(mode="numeric",max_cluster)
+  for (k in 2:(max_cluster+1)){
+    inertia[k-1] = getCumulatedInertieInter(classif,k)
   }
-  k.best=which.max(inertia)
-  plot(inertia,type="b",ylim=c(0,(max(inertia)+5)),cex.lab=1.2,col="grey",axes=F,xlab="Nb. of cluster", ylab="Inertia inter-cluster")
-  axis(1, seq(2,(nrow(data)/4)),lwd=font_size,font.axis=font_size,cex.axis=0.8)
-  axis(2, seq(0,max(inertia)+5,10),lwd=font_size,font.axis=font_size,cex.axis=0.8)
-  text(k.best,max(inertia),paste("optimum",k.best,sep="\n \n"),col="red")
-  points(k.best, max(inertia), pch=21, col="red", cex=1)
-  abline(v=k.best,lty=2,col="red")
-  cat("","Silhouette-optimal number of clusters k =", k.best, "\n","with an inertia of", round(max(inertia),4), "\n")
+  return (inertia)
 }
 
-plotInertiaInter(max_cluster)
+plotCumulatedInertiaInter=function(max_cluster){
+  inertia=getCumulatedInertiaInterPerCluster(max_cluster)
+  k.best=which.max(inertia)
+  plot(inertia,type="b",ylim=c(0,(max(inertia)+5)),cex.lab=1.2,col="grey",axes=F,xlab="Nb. of cluster", ylab="Cumulated inertia inter-cluster")
+  axis(1, seq(2,max_cluster),lwd=font_size,font.axis=font_size,cex.axis=0.8)
+  axis(2, seq(0,max(inertia)+5,10),lwd=font_size,font.axis=font_size,cex.axis=0.8)
+  text(k.best,max(inertia),paste("",round(max(inertia),4),sep="\n \n"),col="red",pos=2)
+  points(k.best, max(inertia), pch=21, col="red", cex=1)
+  abline(v=k.best,lty=2,col="red")
+  cat("","Silhouette-optimal number of clusters k =", k.best, "\n","with a cumulated inter-inertia of", round(max(inertia),4), "\n")
+}
 
+plotCumulatedInertiaInter(max_cluster)
+
+################################
+#          Height difference
+################################
+
+#Show differences between nodes levels (distance between clusters)
+getHeightDifference=function(){
+  height_classif=as.matrix(classif$height)
+  height_diff=matrix(0, length(height_classif), 1)
+  for (i in 2:(length(height_classif))){
+    height_diff[i,]=height_classif[i,]-height_classif[i-1,]
+  }
+  rownames(height_diff)=c((length(height_classif)+1):2)
+  return(height_diff)
+}
+
+#matrix_height=cbind(height_classif,height_diff)
+#colnames(matrix_height)=c("node height","difference")
+
+getRankedHeight=function(){
+  ranked_height_diff=data.frame(getHeightDifference())
+  ranked_height_diff=ranked_height_diff[order(-ranked_height_diff), , drop = FALSE]
+  rownames(ranked_height_diff)
+  return(ranked_height_diff)
+}
+
+printTableInertia=function(){
+  ranked_height_diff=getRankedHeight()
+  matrix_output1=cbind(classif$height,getHeightDifference())
+  for(i in 1:ncol(matrix_output1)) {matrix_output1[,i] = rev(matrix_output1[,i])}
+  matrix_output1=cbind(matrix_output1,getCumulatedInertiaInterPerCluster(nrow(data)-1))
+  rownames(matrix_output1)=seq(2,(nrow(data)))
+  colnames(matrix_output1)=c("Branch height", "Differences","Cumulated inertia")
+  matrix_output1=round(matrix_output1,2)
+  print (matrix_output1)
+}
+
+printTableInertia()
+
+optimal_nb_clusters=as.numeric(rownames(ranked_height_diff)[1])
+#optimal_nb_clusters=nrow(data)-(which.max(getHeightDifference()[-(nrow(data)-1)])-1)
+
+getClusters= function(nb_clusters) {
+  cutree(classif,nb_clusters)
+}
+
+clusters=getClusters(optimal_nb_clusters)
+#clusters<-as.factor(cutree(dendro3,nb_clusters))
+
+getClusterSizes=function(){
+  cluster_sizes=table(clusters)
+  cluster_sizes=data.frame(cluster_sizes)[,2]
+  names(cluster_sizes)=paste("G",seq(1:optimal_nb_clusters),sep="")
+  cluster_sizes=data.frame(cluster_sizes)
+  names(cluster_sizes)="Effectif"
+  return(cluster_sizes)
+}
+cluster_sizes=getClusterSizes()
+print(cluster_sizes)
 
 ################################
 #          COR
@@ -408,7 +384,63 @@ for (k in 2:max_cluster){
     pvdiscriminant[k-1,i]=round(res[i],2)
   }
 }
-#Traitement pour que ça soit beau
-names(pvdiscriminant)=colnames(data)
-df=sort(pvdiscriminant)
 
+
+################################
+#          Dendrogram
+################################
+
+plotDendrogram=function(nb_clusters){
+  par(margin);#x11()
+  plot(classif, ylim=c(0,max(classif$height)),xlim=c(0,nrow(data)),hang=-1, cex.main=2, cex.lab=1.5,lwd=font_size,xlab="Metabolites", sub="",ylab="Distance before each fusion",main="Dendrogram",font.lab=font_size,axes=F)
+  axis(2, seq(0,max(classif$height)),lwd=font_size,font.axis=font_size,cex.axis=0.8)
+  #abline(h=seq(0.0,max(classif$height),0.1), lty=3, col="grey")
+  #abline(h=c(classif$height), lty=3, col="grey")
+  #projection of the clusters
+  rect.hclust(classif, k=nb_clusters, border=rainbow(nb_clusters))
+}
+
+x11();plotDendrogram(as.numeric(optimal_nb_clusters))
+
+################################
+#          Silhouette
+################################
+
+plotAllSilhouette=function(max_cluster){
+  asw <- numeric(max_cluster-1)
+  for (k in 2:(max_cluster-1)) {
+    sil <- silhouette(cutree(classif, k=k), distance_matrix)
+    asw[k] <- summary(sil)$avg.width
+  }
+  
+  k.best <- which.max(asw)
+  # The plot is produced by function plot.silhouette {cluster}
+  plot(1:(max_cluster-1), asw, type="b", xlim=c(2,(max_cluster-1)),ylim=c(0,max(asw)+0.1),col="grey",main="Silhouette-optimal number of clusters, Ward",xlab="Number of groups", ylab="Average silhouette width", axes=F)
+  text(k.best,max(asw),paste("optimum",k.best,sep=":"),col="red", pos=2)
+  axis(1, seq(2,(max_cluster-1)),lwd=font_size,font.axis=font_size,cex.axis=0.8)
+  axis(2, seq(0.0,(max(asw)+0.1),0.1),lwd=font_size,font.axis=font_size,cex.axis=0.8)
+  points(k.best, max(asw), pch=21, col="red", cex=1)
+  cat("","Silhouette-optimal number of clusters k =", k.best, "\n","with an average silhouette width of", round(max(asw),4), "\n")
+  abline(v=k.best,lty=2,col="red")
+}
+
+plotSmallClusterSilhouette=function(max_cluster){
+  par(mfrow=c(2,2))
+  for (k in 3:max_cluster) {
+    sil <- silhouette(cutree(classif, k=k), distance_matrix)
+    silo <- sortSilhouette(sil)
+    rownames(silo) <- row.names(data)[attr(silo,"iOrd")]
+    plot(silo, main=paste("Silhouette plot for",k ,"groups"),cex.names=0.8, col=silo[,1]+1, nmax.lab=100)
+  }
+  par(mfrow=c(1,1))
+}
+
+plotAllSilhouette(40)
+plotSmallClusterSilhouette(max_cluster)
+
+plot(data[,1],data[,2],type="p",pch="+",xlab="X1", ylab="X2",col=c("lightcoral","skyblue","greenyellow")[classif$clustering])
+points(classif$medoids[,1],classif$medoids[,2], cex=1.5,pch=16,col=c("red","blue","green")[1:3])
+s.label(data,boxes=F,clabel=0.8,neig = F,add.plot = T)
+points(data[,1],data[,2],col=c("lightcoral","skyblue","greenyellow")[classif$clustering])
+
+classif$cluster[order(-classif$cluster,decreasing=T)]
