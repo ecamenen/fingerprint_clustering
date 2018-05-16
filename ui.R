2ac8e3576d095b9fdc1c1c768d3efaa58b273f9b
Etienne Camenen <etienne.camenen@gmail.com>
2018-05-16 15:57:39 +0200

    table summary

---
+++ ui.R
@@ -0,0 +1,28 @@
+library(shiny)
+shinyUI( pageWithSidebar(
+  #titlePanel("Hello"),
+  headerPanel("Fingerprint clustering"), #titre appl
+  
+  #pannel de cote
+  sidebarPanel( #elements de controle de l'apli
+    
+    textInput(inputId="infile", #nom (ou ID) associee a cet element de controle,
+                 #sera utilisee dans la partie 'server'
+                 label="Name of the dataset: ", #libellé associee a cet element 
+                 #(apparait au dessus de l'input)
+                 value="matrix.txt")
+  ),
+  
+  mainPanel(
+    #titre donne a la partie presentant les sorties
+    h3("Silhouette plot: "),
+    
+    #va permettre d'afficher une sortie de type 'texte'
+    #le contenu sera defini dans la partie server avec comme nom de variable
+    #'sortie de texte'
+    #plotOutput("silhouette")
+    tableOutput("silhouette")
+    #en sortie, tu attend du texte
+  )
+  
+))
