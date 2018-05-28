b8015195f3a67bde268f58d31d7d3976d1b84aee
Etienne Camenen <etienne.camenen@gmail.com>
2018-05-28 16:15:19 +0200

    method classif par

--- server.R
+++ server.R
@@ -1,8 +1,10 @@
 library(shiny)
 source("fingerprint_clustering.R")
+classif_methods = list("K-menoids" = 1,  "K-means" = 2, "Ward"=3, "Complete links"=4, "Single links"=5, "UPGMA"=6, "WPGMA"=7, "WPGMC"=8, "UPGMC"=9)
+
 
 shinyServer(function(input,output){ 
-  
+
   loadData = function(infile){
     data = read.table(infile, header=F, sep="\t", dec=".", row.names=1)
     colnames(data) <- substr(rownames(data), 1, 25) -> rownames(data)
@@ -10,21 +12,22 @@ shinyServer(function(input,output){
     return (data)
   }
   
-  performClassif = function(data){
-    #Perform classification
-    if(classif_type == 0) classif_type = selectBestCAH(data, F)
-    return(getCAH(data, classif_type))
+  performClassif = function(input){
+    data = loadData(input$infile)
+    return (getCAH(data, getClassifValue(input$classif_type)))
   }
+  
+  getClassifValue = function(key)  unlist(classif_methods[key])
   
   output$summary = renderTable({
     data = loadData(input$infile)
-    classif = performClassif(data)
+    classif = getCAH(data, getClassifValue(input$classif_type))
     printSummary(classif_type, max_cluster, classif, data)
   })
   
   output$fusion_levels = renderPlot({
     data = loadData(input$infile)
-    classif = performClassif(data)
+    classif = getCAH(data, getClassifValue(input$classif_type))
     plotFusionLevels(classif_type, max_cluster, classif, data)
   })
 })
--- ui.R
+++ ui.R
@@ -1,4 +1,12 @@
 library(shiny)
+classif_methods = list("K-menoids" = 1,  "K-means" = 2, "Ward"=3, "Complete links"=4, "Single links"=5, "UPGMA"=6, "WPGMA"=7, "WPGMC"=8, "UPGMC"=9)
+
+#min-max: minimum and maximum position number in the list
+getClassifKeys = function(min, max)
+  #similar to for i in min:max
+  unlist(sapply(min:max, function(i) names(classif_methods)[i]))
+
+
 shinyUI( pageWithSidebar(
   #titlePanel("Hello"),
   headerPanel("Fingerprint clustering"), #titre appl
@@ -8,9 +16,20 @@ shinyUI( pageWithSidebar(
     
     textInput(inputId="infile", #nom (ou ID) associee a cet element de controle,
                  #sera utilisee dans la partie 'server'
-                 label="Name of the dataset: ", #libellé associee a cet element 
+                 label=h4("Name of the dataset: "), #libellé associee a cet element 
                  #(apparait au dessus de l'input)
-                 value="matrix.txt")
+                 value="matrix.txt"),
+    
+    #checkValues and checkNames do not works on selectInput but only on checkbox
+    selectInput(inputId="classif_type",
+                 label=h4("Méthode de classification: "),
+                 selected = "Complete links",
+                 choices = list(`Partitionning clustering` = c(names(classif_methods)[1], names(classif_methods)[2]), 
+                                `Hierarchical clustering` = getClassifKeys(3,9)))
+
+    #names(classif_methods)[2]
+    #unlist(classif_methods[2])
+    
   ),
   
   mainPanel(
