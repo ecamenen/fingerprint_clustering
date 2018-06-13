598bf7407c815533634cc1d54ae61ecb6df7b7ea
Etienne Camenen <etienne.camenen@gmail.com>
2018-06-13 12:06:14 +0200

    changed archive

--- launcher.R
+++ launcher.R
@@ -137,7 +137,7 @@ for (l in librairies){
 }
 source("fingerprint_clustering.R") 
 
-install.packages("ade4", contrib = "https://pbil.univ-lyon1.fr/CRAN/src/contrib/Archive/ade4/")
+install.packages("ade4", contrib = "https://pbil.univ-lyon1.fr/CRAN/src/contrib/Archive/")
 library(ade4)
 
 #Get arguments
