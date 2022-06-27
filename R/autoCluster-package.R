5344ded34283eb60eb323c2fb7bb51a343152a8c
ecamenen <etienne.camenen@gmail.com>
2022-06-27 20:03:36 +0200

    Add configuration for package

---
+++ R/autoCluster-package.R
@@ -0,0 +1,5 @@
+#' @import ade4 cluster config desc gclus ggplot2 golem magrittr openxlsx
+#' @import optparse scales shiny graphics grDevices stats utils
+#' @rawNamespace import(shinyjs, except = runExample)
+#' @keywords internal
+"_PACKAGE"
